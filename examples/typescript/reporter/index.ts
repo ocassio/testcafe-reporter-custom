481bc2bf8ed5ea4a009bc984dd8ed1e69646e310
Alexander Ionov <orlicus@gmail.com>
2020-01-09 19:17:05 +0400

    External configuration and TypeScript support (#2)
    External configuration and TypeScript support

---
+++ examples/typescript/reporter/index.ts
@@ -0,0 +1,23 @@
+module.exports = function () {
+    return {
+        async reportTaskStart(): Promise<void> {
+            this.write('Task has been started').newline();
+        },
+
+        async reportFixtureStart(name: string): Promise<void> {
+            this.write(`Fixture "${name}" has been started`).newline();
+        },
+
+        async reportTestStart(name: string): Promise<void> {
+            this.write(`Test "${name}" has been started`).newline();
+        },
+
+        async reportTestDone(name: string): Promise<void> {
+            this.write(`Test "${name}" has been finished`).newline();
+        },
+
+        async reportTaskDone(): Promise<void> {
+            this.write('Task has been completed').newline();
+        }
+    };
+}
