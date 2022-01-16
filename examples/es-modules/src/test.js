49a8d958df9ab1fbe4bd43c9771a17ad5b47c0ff
Alexander Ionov <orlicus@gmail.com>
2022-01-16 19:06:58 +0400

    fix: ES Modules support for basic cases

---
+++ examples/es-modules/src/test.js
@@ -0,0 +1,5 @@
+fixture`Getting Started`.page`http://devexpress.github.io/testcafe/example`;
+
+test("My first test", async t => {
+  await t.typeText("#developer-name", "John Smith").click("#submit-button");
+});
