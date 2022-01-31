01eca0b264557095b4325e7da7767857ff3b0dba
Alexander Ionov <orlicus@gmail.com>
2022-01-31 13:28:47 +0400

    fix: Custom fields usage (#12)

--- examples/basic/reporter/index.js
+++ examples/basic/reporter/index.js
@@ -1,23 +1,28 @@
 module.exports = function () {
-    return {
-        async reportTaskStart() {
-            this.write('Task has been started').newline();
-        },
+  return {
+    async reportTaskStart() {
+      this.write("Task has been started").newline();
+    },
 
-        async reportFixtureStart(name) {
-            this.write(`Fixture "${name}" has been started`).newline();
-        },
+    async reportFixtureStart(name) {
+      this.write(`Fixture "${name}" has been started`).newline();
+    },
 
-        async reportTestStart(name) {
-            this.write(`Test "${name}" has been started`).newline();
-        },
+    // async reportTestStart(name) {
+    //   this.write(`Test "${name}" has been started`).newline();
+    // },
 
-        async reportTestDone(name){
-            this.write(`Test "${name}" has been finished`).newline();
-        },
+    async reportTestDone(name) {
+      this.write(`Test "${name}" has been finished`).newline();
+    },
 
-        async reportTaskDone() {
-            this.write('Task has been completed').newline();
-        }
-    };
-}
+    async reportTaskDone() {
+      this._privateFunction();
+      this.write("Task has been completed").newline();
+    },
+
+    _privateFunction() {
+      this.write("Private function call").newline();
+    },
+  };
+};
